6ad9a3510f3312b907550d6f096440cc936890dc
leowzhenkang <leowzk@hotmail.com>
2019-12-17 09:24:51 +0800

    part 1 done

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display( currentInput );
 };
-
+var totalPineapple = ""
+var emoji = "🍍";
 var display = function(stuffToDisplay){
+    if(stuffToDisplay==="clear"){
+    document.getElementById('output').innerHTML=""
+    }
   // your DOM manipulation code here
-  
+    var displayPineapple = document.createElement("P")
+    for (var i = 0; i < stuffToDisplay; i++) {
+        totalPineapple += emoji;
+        };
+    displayPineapple.innerText = (totalPineapple);
+    document.querySelector("#output").appendChild(displayPineapple);
+    totalPineapple = ""
+    document.querySelector('#input').value = ""
 };
